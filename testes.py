ad379a1f0338b9db41ff52a94baaceb2bae4c506
Jabes Christian <jabescristian08@gmail.com>
2025-07-15 15:40:11 -0300

    feat: atualiza lógica de testes

--- testes.py
+++ testes.py
@@ -3,7 +3,7 @@
 import requests
 
 headers = {
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3IiwiZXhwIjoxNzUyODc5MjA0fQ.fOpwQY9AubVmWP8z4lnChs3K0KUbaty49bjsvHlAHfw"
+    "Authorization": "Bearer Seu token refresh"
 }
 requisicao = requests.get("http://127.0.0.1:8000/auth/refresh", headers=headers)
 print(requisicao)
